c171271531a9f2e86c91e108ed94c6b3b5e9d179
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-26 18:33:29 -0500

    lint

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  * Licensed under the MIT License.
  */
 
-
 module.exports = function isGitUrl(str) {
   var re = /(?:git|ssh|https?|git@[\w\.]+):(?:\/\/)?[\w\.@:\/~_-]+\.git\/?/;
   return re.test(str);
